53ce2ed72efd023991a4a913c8807468ffde6f0b
Ovidiu Mazuru <ovidiu.mazuru@gmail.com>
2024-09-11 23:40:41 +0300

    initial commit

---
+++ lib/garden_fusion.ex
@@ -0,0 +1,18 @@
+defmodule GardenFusion do
+  @moduledoc """
+  Documentation for `GardenFusion`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GardenFusion.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/garden_fusion_test.exs
@@ -0,0 +1,8 @@
+defmodule GardenFusionTest do
+  use ExUnit.Case
+  doctest GardenFusion
+
+  test "greets the world" do
+    assert GardenFusion.hello() == :world
+  end
+end
